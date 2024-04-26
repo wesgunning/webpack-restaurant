84281df2660dfb5f55a9fa2887f6c58817cb8fe1
Wes <108998223+wesgunning@users.noreply.github.com>
2024-04-26 12:48:44 -0400

    Move js for dynamic load of homepage content into 'home' function within main.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,3 @@
 import './styles.css'
-
-const content = document.getElementById('content');
-const contentHeader = document.createElement('div');
-contentHeader.id = ('content-header');
-content.appendChild(contentHeader);
-const contentH1 = document.createElement('h1');
-contentH1.innerText = "Gianni's Artisan Pizzeria";
-contentHeader.appendChild(contentH1);
-const about = document.createElement('div');
-about.classList.add('about');
-content.appendChild(about);
-const aboutPara = document.createElement('p');
-aboutPara.innerText = "At Giannis's we believe in using high-quality, fresh ingredients along with our time-honored family traditions to bring you modern pizzas using authentic cooking techniques.";
-about.appendChild(aboutPara);
-const hours = document.createElement('div');
-hours.id = ('hours');
-const hoursPara = document.createElement('p');
-hoursPara.innerHTML = "Mon-Thurs: 9:00am - 7:00pm <br>Fri - Sat: 9:00am - 10:00pm <br>Sun: 11:00am - 7:00pm";
-content.appendChild(hours);
-hours.appendChild(hoursPara);
-const address = document.createElement('div');
-address.id = ('address');
-const addressH2 = document.createElement('h2');
-addressH2.innerText = 'Address';
-content.appendChild(address);
-address.appendChild(addressH2);
-const addressPara = document.createElement('p');
-addressPara.innerText = "123 Pepperoni Pl, Boston, MA";
-address.appendChild(addressPara);
+import {home} from './main.js';
+home();
